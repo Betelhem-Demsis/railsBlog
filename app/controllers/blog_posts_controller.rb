2c9b326968c83e932dbd6a049434a59f4d6353f8
betelhem demsis <bettydemsis@gmail.com>
2024-11-12 11:51:51 +0300

    Add: user authentication

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,16 +1,16 @@
 class BlogPostsController < ApplicationController
+  before_action :authenticate_user!, except: [ :index, :show ]
+  before_action :set_blog_post, except: [ :index, :new, :create ]
+
   def index
     @blog_posts= BlogPost.all
   end
 
   def show
-    @blog_post= BlogPost.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
   end
 
   def new
-    @blog_post = BlogPost.new
+  @blog_post = BlogPost.new
   end
 
   def create
@@ -23,11 +23,9 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    @blog_post = BlogPost.find(params[:id])
   end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to @blog_post
     else
@@ -36,7 +34,7 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
+  
     @blog_post.destroy
     redirect_to root_path
   end
@@ -47,4 +45,13 @@ class BlogPostsController < ApplicationController
     params.require(:blog_post).permit(:title, :body)
   end
 
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
+
+  def authenticate_user!
+    redirect_to new_user_session_path, alert: "u must sign in o sign up to continue" unless user_signed_in?
+  end
 end
